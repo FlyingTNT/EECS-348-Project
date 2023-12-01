614dd6cb764fa88caedaea4a780642351744a22d
soulsoiledit <30585327+soulsoiledit@users.noreply.github.com>
2023-11-30 22:26:57 -0600

    Add documentation comments for evaluator

--- Source/Control/Evaluator.cpp
+++ Source/Control/Evaluator.cpp
@@ -1,32 +1,50 @@
+/**
+ * Evaluator.cpp
+ * Purpose: Implement the class defined in Evaluator.hpp
+ * Author: Aryan Kevat
+ * Date Created: 2023-11-29
+*/
 
+#include "Evaluator.hpp"
 #include "../Datastructures/List.hpp"
 #include "../EquationElements/EquationElement.hpp"
 #include "../EquationElements/UnaryOperator.hpp"
 #include "../EquationElements/BinaryOperator.hpp"
 #include "../EquationElements/NumericValue.hpp"
 #include "../EquationElements/Parentheses.hpp"
-#include "Evaluator.hpp"
 #include <iostream>
 
+
+/**
+ * Finds the index of the next operation to perform
+ * @param equation the list of elements representing the current equation
+ * @returns the highest priority index or the next unary operator
+*/
 static int getHighestPriorityIndex(List<EquationElement>* equation) {
+  // initialize maximum and index
   int maximum = -1;
   int index = -1;
+  // iterate over each element in the equation
   for (int i = 0; i < equation->length(); i++) {
-    int priority = (*equation->get(i)).getPriority();
+    // retrieve priority of the current element
+    int priority = equation->get(i)->getPriority();
+    // update maximum and index if the current priority is greater
     if (priority > maximum) {
       maximum = priority;
       index = i;
     }
   }
 
-  // Find the last unary operator, if it exists
-  EquationElement* oper = equation->get(index);
-  if (oper->getPriority() >= 2 && oper->getPriority() < 5) {
+  // find the last unary operator, if it exists
+  // check if the current priority is of UnaryOperator or higher and under parentheses
+  if (maximum >= 2 && maximum < 5) {
+    // iterate over each element in the current equation starting after the maximum's index
     for (int i = index + 1; i < equation->length(); i++) {
-      int priority = (*equation->get(i)).getPriority();
-      if (priority != 2) {
+      // break if the next element's priority isn't of UnaryOperator
+      if (equation->get(i)->getPriority() != 2) {
         break;
       }
+      // Otherwise, increments index
       index += 1;
     }
   }
@@ -34,47 +52,65 @@ static int getHighestPriorityIndex(List<EquationElement>* equation) {
   return index;
 }
 
-static void printList(List<EquationElement>* equation) {
-  for (int i = 0; i < equation->length(); i++) {
-    std::cout << *equation->get(i) << " ";
-  }
-  std::cout << "\n";
-}
-
+/**
+ * Finds the index of the next operation to perform
+ * @param equation the list of elements representing the current equation
+ * @returns the value of the given expression equation
+ * @throws runtime error if an operator is missing an operand
+*/
 int Evaluator::evaluate(List<EquationElement>* equation) {
   while (equation->length() > 0) {
+    // get index of highest priority element
     int index = getHighestPriorityIndex(equation);
-
+    // get corresponding operator at index
     EquationElement* oper = equation->get(index);
+    int priority = oper->getPriority();
 
-    if (oper->getPriority() == 5) {
+    // if the operator is parens, recurse into evaluate() with getValue call
+    if (priority == 5) {
       equation->set(index, new NumericValue(oper->getValue(nullptr, nullptr)));
     }
 
     try {
-      if (oper->getPriority() == 2) {
+      // check if priority is of NumericValue
+      // essentially means the evaluation is finished
+      if (priority == 0) {
+        // return value of current numeric element
+        return equation->pop()->getValue(0, 0);
+      // check if priority is of UnaryOperator
+      } else if (priority == 2) {
+        // throw if no element is after it
         if (index + 1 == equation->length()) {
           throw std::runtime_error("");
         }
 
+        // get next element
         EquationElement* next = equation->get(index + 1);
 
+        // set element at current index to numeric value of evaluated unary operation
         equation->set(index, new NumericValue(oper->getValue(nullptr, next)));
 
+        // remove the following element
         equation->remove(index + 1);
+      // Remaining elements must be of BinaryOperator
+      } else {
+        // throw if the element has nothing before or after it
+        if (index == 0 || index + 1 == equation->length()) {
+          throw std::runtime_error("");
+        }
 
-      } else if (oper->getPriority() == 1 || oper->getPriority() > 2) {
+        // get operators before and after the current element
         EquationElement* prev = equation->get(index - 1);
         EquationElement* next = equation->get(index + 1);
+        // set element at current index to numeric value of evaluated binary operation
         equation->set(index, new NumericValue(oper->getValue(prev, next)));
+        // remove previous and next elements
         equation->remove(index + 1);
         equation->remove(index - 1);
-      } else {
-        return equation->pop()->getValue(0, 0);
       }
     } catch (std::runtime_error &e) {
+      // throw missing operand message
       throw std::runtime_error(oper->getSymbol() + " is missing an operand!");
-      break;
     }
   }
 }
--- Source/Control/Evaluator.hpp
+++ Source/Control/Evaluator.hpp
@@ -1,9 +1,18 @@
+/**
+ * Evaluator.cpp
+ * Purpose: A class that drives the main logic for evaluating an expression
+ * Author: Aryan Kevat
+ * Date Created: 2023-11-29
+*/
+
 #ifndef EVALUATOR_H
 #define EVALUATOR_H
 #include "../Datastructures/List.hpp"
 #include "../EquationElements/EquationElement.hpp"
-#include <string>
 
+/**
+ * Contains lonely function to evaluate a list of EquationElement
+*/
 class Evaluator
 {
     public:
