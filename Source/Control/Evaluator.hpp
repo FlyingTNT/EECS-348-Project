3a9bcd7cea50c47aab49d500007c2cc02f171ed1
soulsoiledit <30585327+soulsoiledit@users.noreply.github.com>
2023-11-30 22:26:59 -0600

    Add evaluator

---
+++ Source/Control/Evaluator.hpp
@@ -0,0 +1,13 @@
+#ifndef EVALUATOR_H
+#define EVALUATOR_H
+#include "../Datastructures/List.hpp"
+#include "../EquationElements/EquationElement.hpp"
+#include <string>
+
+class Evaluator
+{
+    public:
+    static int evaluate(List<EquationElement>&);
+};
+
+#endif
